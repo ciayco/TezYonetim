54de7b1e0348153b073428c9cf4614079e32cce8
İslamAkgüç <slamAkg@DESKTOP-DSTBR8B>
2017-01-11 18:28:30 +0300

    Masterpage yapıldı

--- TezYonetim/Default.aspx.cs
+++ TezYonetim/Default.aspx.cs
@@ -10,7 +10,7 @@ public partial class Login : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["Id"] != null)
-        {             
+        {
             if ((int)Session["derece"] == 1) //1 veritabanında Admin/Hoca demek
             {
                 Response.Redirect(@"~/Admin.aspx");
@@ -19,50 +19,53 @@ public partial class Login : System.Web.UI.Page
             {
                 Response.Redirect(@"~/User.aspx");
             }
-        }       
+        }
     }
     protected void btnGiris_Click(object sender, EventArgs e)
     {
         string username = Request["username"].Trim();
-        TezDBEntities db = new TezDBEntities();
-        if (db.Ogrenci.Where(w => w.No == username).Any())
-        {
-            string sifre = Sifreleme.Sifrele(Request["pass"].Trim());
-            Ogrenci Ogrenci = db.Ogrenci.Where(w => w.No == username && w.Sifre == sifre).FirstOrDefault();
-            if (Ogrenci != null)
-            {
-                AppKontrol.id = (int)Ogrenci.Id; //Id kontrolu           
-                AppKontrol.derece = (int)Ogrenci.Derece;// derece kontrolü
-                AppKontrol.name = Ogrenci.Ad;// isim soyisim kontrolü
-                Response.Cookies.Add(cookie.Cookie(Ogrenci.No, Ogrenci.Sifre));             
-                Response.Redirect(@"~/User.aspx");                           
-            }else
-            {
-                Label1.Text = "Kullanıcı adı veya Şifre Uyuşmadı!";
-            }
-        }
-        else if (db.Hoca.Where(w => w.Mail == username).Any())
-        {
-            string sifre = Sifreleme.Sifrele(Request["pass"].Trim());
-            Hoca hoca = db.Hoca.Where(w => w.Mail == username && w.Sifre == sifre).FirstOrDefault();
-            if (hoca != null)
-            {
-                AppKontrol.id = (int)hoca.Id; //Id kontrolu           
-                AppKontrol.derece = (int)hoca.Derece;// derece kontrolü
-                AppKontrol.name = hoca.Ad;// isim soyisim kontrolü
-                Response.Cookies.Add(cookie.Cookie(hoca.Mail, hoca.Sifre));   
-                Response.Redirect(@"~/Admin.aspx");
-                
-             }
-            else
-            {
-                Label1.Text = "Kullanıcı adı veya Şifre Uyuşmadı!";
-            }
-        }
 
-        else
-        {
-            Label1.Text = "Kullanıcı adı veya Şifre Uyuşmadı!";
-        }
+        TezDBEntities db = new TezDBEntities();
+       
+
+            if (db.Ogrenci.Where(w => w.No == username).Any())
+            {
+                string sifre = Sifreleme.Sifrele(Request["pass"].Trim());
+                Ogrenci Ogrenci = db.Ogrenci.Where(w => w.No == username && w.Sifre == sifre).FirstOrDefault();
+                if (Ogrenci != null)
+                {
+                    AppKontrol.id = (int)Ogrenci.Id; //Id kontrolu           
+                    AppKontrol.derece = (int)Ogrenci.Derece;// derece kontrolü
+                    AppKontrol.name = Ogrenci.Ad;// isim  kontrolü
+                    Response.Cookies.Add(cookie.Cookie(Ogrenci.No, Ogrenci.Sifre));
+                    Response.Redirect(@"~/User.aspx");
+                }
+                else
+                {
+                    Label1.Text = "Kullanıcı adı veya Şifre Uyuşmadı!";
+                }
+            }
+     
+       
+           else if (db.Hoca.Where(w => w.Mail == username).Any())
+            {
+                string sifre = Sifreleme.Sifrele(Request["pass"].Trim());
+                Hoca hoca = db.Hoca.Where(w => w.Mail == username && w.Sifre == sifre).FirstOrDefault();
+                if (hoca != null)
+                {
+                    AppKontrol.id = (int)hoca.Id; //Id kontrolu           
+                    AppKontrol.derece = (int)hoca.Derece;// derece kontrolü
+                    AppKontrol.name = hoca.Ad;// isim soyisim kontrolü
+                    Response.Cookies.Add(cookie.Cookie(hoca.Mail, hoca.Sifre));
+                    Response.Redirect(@"~/Admin.aspx");
+
+                }
+                else
+                {
+                    Label1.Text = "Kullanıcı adı veya Şifre Uyuşmadı!";
+                }
+            }
+       
+        Label1.Text = "Kullanıcı adı veya Şifre Uyuşmadı!";
     }
 }
