d7ab59eeb01b5b38452ec0cf27a2229e55300841
İslamAkgüç <slamAkg@DESKTOP-DSTBR8B>
2017-03-25 14:15:52 +0300

    dasd

--- TezYonetim/Forms/Admin/MasterPageAdmin.master.cs
+++ TezYonetim/Forms/Admin/MasterPageAdmin.master.cs
@@ -9,6 +9,10 @@ public partial class Forms_Admin_MasterPageAdmin : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["id"] == null)
+        {
+            Response.Redirect(@"~/Default.aspx");
+        }
         Label1.Text = Session["name"].ToString();
     }
     protected void LogOut_Click(object sender, EventArgs e)
