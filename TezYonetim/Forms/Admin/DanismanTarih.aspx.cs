0ceed689386383330a36a24d3a1f97933c9bc609
Ciayco <ciayco07@gmail.com>
2017-03-25 15:19:58 +0300

    admin tarih..

--- TezYonetim/Forms/Admin/DanismanTarih.aspx.cs
+++ TezYonetim/Forms/Admin/DanismanTarih.aspx.cs
@@ -7,13 +7,29 @@ using System.Web.UI.WebControls;
 
 public partial class Forms_Admin_DanismanTarih : TezBaseAdmin
 {
+        TezDBEntities db;
+        Sistem trh;
     protected void Page_Load(object sender, EventArgs e)
     {
-       
+        db = new TezDBEntities();
+        trh = db.Sistem.Where(q => q.Id == 1).FirstOrDefault();
+      
+        Label1.Text = "Başlangıç " + trh.DanismanSBas.ToString();
+        Label2.Text = "Bitiş " + trh.DanismanSBit.ToString();
     }
     protected void btnGiris_Click(object sender, EventArgs e)
     {
-        Label1.Text = DBas.Text.Replace("T", " ");
-        Label2.Text = DBit.Text.Replace("T", " ");
+
+        //DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss").Replace(' ', 'T');
+        //Tarih kayıt
+        trh.DanismanSBas = DateTime.Parse(DBas.Text.Replace("T", " "));
+        trh.DanismanSBit = DateTime.Parse(DBit.Text.Replace("T", " "));
+        db.SaveChanges();
+
+        //Label yenileme
+        Label1.Text = "Başlangıç " + trh.DanismanSBas.ToString();
+        Label2.Text = "Bitiş " + trh.DanismanSBit.ToString();
+ 
+
     }
 }
--- TezYonetim/Forms/Admin/TezTarih.aspx.cs
+++ TezYonetim/Forms/Admin/TezTarih.aspx.cs
@@ -7,12 +7,28 @@ using System.Web.UI.WebControls;
 
 public partial class Forms_Admin_TezTarih : System.Web.UI.Page
 {
+    TezDBEntities db;
+    Sistem trh;
     protected void Page_Load(object sender, EventArgs e)
     {
+        db = new TezDBEntities();
+        trh = db.Sistem.Where(q => q.Id == 1).FirstOrDefault();
 
+        Label1.Text = "Başlangıç " + trh.TezSBas.ToString();
+        Label2.Text = "Bitiş " + trh.TezSBit.ToString();
     }
     protected void btnGiris_Click(object sender, EventArgs e)
     {
+        //DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss").Replace(' ', 'T');
+        //Tarih kayıt
+        trh.TezSBas = DateTime.Parse(TBas.Text.Replace("T", " "));
+        trh.TezSBit = DateTime.Parse(TBit.Text.Replace("T", " "));
+        db.SaveChanges();
+
+        //Label yenileme
+        Label1.Text = "Başlangıç " + trh.TezSBas.ToString();
+        Label2.Text = "Bitiş " + trh.TezSBit.ToString();
+
 
     }
 }
