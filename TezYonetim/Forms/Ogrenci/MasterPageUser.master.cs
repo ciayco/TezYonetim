e8cea8c73c82d18938ccd16b4286724b958279c3
İslamAkgüç <slamAkg@DESKTOP-DSTBR8B>
2017-04-01 22:02:38 +0300

    Master page session guncellemelerı

--- TezYonetim/Forms/Ogrenci/MasterPageUser.master.cs
+++ TezYonetim/Forms/Ogrenci/MasterPageUser.master.cs
@@ -9,6 +9,10 @@ public partial class MasterPageUser : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["id"] == null)
+        {
+            Response.Redirect(@"~/Default.aspx");
+        }
         Label1.Text = Session["name"].ToString();
     }
     protected void LogOut_Click(object sender, EventArgs e)
