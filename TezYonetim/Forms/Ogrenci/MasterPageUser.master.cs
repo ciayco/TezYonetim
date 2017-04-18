b4517764e0545099534856253059b5c8e6b9488f
İslamAkgüç <slamAkg@DESKTOP-DSTBR8B>
2017-04-18 23:03:19 +0300

    linkleri yok etmke fonk yazıldı user masterpage e

--- TezYonetim/Forms/Ogrenci/MasterPageUser.master.cs
+++ TezYonetim/Forms/Ogrenci/MasterPageUser.master.cs
@@ -14,11 +14,25 @@ public partial class MasterPageUser : System.Web.UI.MasterPage
             Response.Redirect(@"~/Default.aspx");
         }
         Label1.Text = Session["name"].ToString();
+
+        Kontrol();
     }
     protected void LogOut_Click(object sender, EventArgs e)
     {
         Response.Cookies["MyCookie"].Expires = DateTime.Now.AddDays(-1);
         Session.RemoveAll();
         Response.Redirect(@"~/Default.aspx");
+    }
+    protected void Kontrol()
+    {
+        TezDBEntities db = new TezDBEntities();
+        DateTime tarih = DateTime.Now;
+        Sistem trh = db.Sistem.Where(q => q.Id == 1).FirstOrDefault();
+        if (tarih >= trh.DanismanSBas && tarih <= trh.DanismanSBit)
+        {
+            TezHocaSec.Visible = false;
+            TezSec.Visible = false;
+        }
+
     }
 }
